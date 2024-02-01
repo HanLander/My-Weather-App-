0275bb6149b0f0cbc6784f1d2e6edfbb6e90d946
HanLander <157143716+HanLander@users.noreply.github.com>
2024-02-01 20:43:54 +0000

    Added calling time and icon functions and added CSS tweaks

--- src/index.js
+++ src/index.js
@@ -57,6 +57,12 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "d4c90a6e0890oeaa244ft334db3e3663";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -66,25 +72,28 @@ function getForecast(city) {
 function displayForecast(response) {
   console.log(response.data);
 
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat,"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
          <div class="weather-forecast-day">
-          <div class="weather-forecast-date">${day}</div>
-          <div class="weather-forecast-icon">☁️</div>
+          <div class="weather-forecast-date">${formatDay(day.time)}</div>
+          <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
           <div class="weather-forecast-temperature">
             <span class="weather-forecast-temperature-max">
-              <strong>18°C</strong></span
+              <strong>${Math.round(day.temperature.maximum)}°C   </strong></span
             >
-            <span class="weather-forecast-temperature-min"> 12°C</span>
+            <span class="weather-forecast-temperature-min">${Math.round(
+              day.temperature.minimum
+            )}°C   </span>
           </div>
           </div>
-          
+            
 `;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
